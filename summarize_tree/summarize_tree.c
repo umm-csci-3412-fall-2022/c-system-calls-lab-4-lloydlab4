83ee4a07c096fd5396d52c81021ef1247a707acf
Lloyd Hilsgen <hilsg008@neoprene.morris.umn.edu>
2022-11-08 16:11:27 -0600

    Using stat

--- summarize_tree/summarize_tree.c
+++ summarize_tree/summarize_tree.c
@@ -5,19 +5,24 @@
 #include <dirent.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
 
 static int num_dirs, num_regular;
 
 bool is_dir(const char* path) {
-  /*
-   * Use the stat() function (try "man 2 stat") to determine if the file
-   * referenced by path is a directory or not.  Call stat, and then use
-   * S_ISDIR to see if the file is a directory. Make sure you check the
-   * return value from stat() in case there is a problem, e.g., maybe the
-   * the file doesn't actually exist.
-   */
+  
+  struct stat buf;
+  
+  int not_exist = stat(path, &buf);
+  if(not_exist == 0) {
+    return S_ISDIR(buf.st_mode);
+  } else {
+    char* error_message = (char*) path;
+    strcat(error_message, " does not exist");
+    perror(path);
+    exit(1);
+  }
 }
-
 /* 
  * I needed this because the multiple recursion means there's no way to
  * order them so that the definitions all precede the cause.
@@ -25,24 +30,33 @@ bool is_dir(const char* path) {
 void process_path(const char*);
 
 void process_directory(const char* path) {
-  /*
-   * Update the number of directories seen, use opendir() to open the
-   * directory, and then use readdir() to loop through the entries
-   * and process them. You have to be careful not to process the
-   * "." and ".." directory entries, or you'll end up spinning in
-   * (infinite) loops. Also make sure you closedir() when you're done.
-   *
-   * You'll also want to use chdir() to move into this new directory,
-   * with a matching call to chdir() to move back out of it when you're
-   * done.
-   */
+  num_dirs++;
+  DIR *dir = opendir(path);
+  chdir(path);
+  
+  struct dirent *entry;
+  errno = 0;
+  while(((entry = readdir(dir)) != NULL) && errno == 0) {
+    if(is_dir(entry->d_name)) {
+      if(strcmp(entry->d_name, "..") != 0 && strcmp(entry->d_name, ".") != 0) {
+        process_directory(entry->d_name);
+      }
+    } else {
+      num_regular++;
+    }
+  }
+  if(errno != 0) {
+    char* error_message = (char*) path;
+    strcat(error_message, " returned error");
+    perror(path);
+    exit(1);
+  } else {
+    chdir("..");
+  }
 }
 
 void process_file(const char* path) {
-  /*
-   * Update the number of regular files.
-   * This is as simple as it seems. :-)
-   */
+  num_regular++;
 }
 
 void process_path(const char* path) {
