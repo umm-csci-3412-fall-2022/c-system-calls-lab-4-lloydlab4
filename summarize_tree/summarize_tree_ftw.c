3ce772cc13e46178f355a8993229b4a06dc61f79
Lloyd Hilsgen <hilsg008@mylar.morris.umn.edu>
2022-11-17 15:29:34 -0600

    That was easy

---
+++ summarize_tree/summarize_tree_ftw.c
@@ -0,0 +1,31 @@
+#include<ftw.h>
+#include<stdio.h>
+
+
+static int num_dirs, num_regular;
+
+static int callback(const char *fpath, const struct stat *sb, int typeflag) {
+    if(typeflag == FTW_F) num_regular++;
+    if(typeflag == FTW_D) num_dirs++;
+    return 0;
+}
+
+#define MAX_FTW_DEPTH 16
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+    printf ("Usage: %s <path>\n", argv[0]);
+    printf ("       where <path> is the file or root of the tree you want to summarize.\n");
+    return 1;
+  }
+
+  num_dirs = 0;
+  num_regular = 0;
+  
+  ftw(argv[1], callback, MAX_FTW_DEPTH);
+
+  printf("There were %d directories.\n", num_dirs);
+  printf("There were %d regular files.\n", num_regular);
+
+  return 0;
+}
